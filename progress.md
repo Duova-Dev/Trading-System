6f2722420c75f6aeca8b88a1cec044358c3af809
Jimmy Kang <jimmykang1016@gmail.com>
2020-05-25 15:14:42 -0700

    create progress.md

---
+++ progress.md
@@ -0,0 +1,18 @@
+# Progress doc
+
+What has been done, what is being done, what must be done
+
+## What has been done
+- Websocket integration for data
+- Separation of threads between receiving data and acting on it
+
+## What is being done
+- Verify REST calls to order interface
+
+## What must be done
+- Implement separation of API keys and code
+- Basic strategies
+    - Market making
+    - Simple mean reversion
+- Separation of system and algorithm code
+-  
--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,11 @@ use std::sync::mpsc::{Sender, Receiver};
 use std::thread;
 use std::time::Duration;
 use json;
+use colour;
+use csv::Writer;
 
-fn main() {
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (tx, rx): (Sender<String>, Receiver<String>) = mpsc::channel();
 
     // thread to pull live trade data from binance
@@ -16,12 +19,48 @@ fn main() {
         interface::live_trade_stream("btcusdt@trade", &tx);
     });
 
+    let mut wtr = Writer::from_path("data/10minutechunk.csv")?;
+    wtr.write_record(&["timestamp", "price,", "quantity", "buyermm"])?;
+
+    let mut counter = 0;
+    let mut delta_time: u64;
+    let mut initial_time: u64 = 0;
+    let mut first_run_occured = false;
     // loop to constantly process trading data
     for received in rx {
-        let parsed = json::parse(&received).unwrap();
-        println!("{}", parsed["p"]);
-    }
-    
-    trades_thread.join().unwrap();
+        let trade = json::parse(&received).unwrap();
+        // check if timestamp is present
+        if trade["E"].is_null() {
+            continue;
+        }
+        if first_run_occured == false {
+            println!("initial trade: {}", trade["E"]);
+            initial_time = trade["E"].as_u64().unwrap();
+            first_run_occured = true;
+        }
 
+        delta_time = trade["E"].as_u64().unwrap() - initial_time;
+
+        // blue means buy order put out, red means sell order put out
+        if trade["m"] == true {
+            colour::red_ln!("{}", trade["p"]);
+        } else {
+            colour::blue_ln!("{}", trade["p"]);
+        }
+        wtr.write_record(&[
+            trade["E"].to_string(), 
+            trade["p"].to_string(),
+            trade["q"].to_string(),
+            trade["m"].to_string(),
+        ])?;
+
+        counter += 1;
+        println!("delta_time: {}", delta_time);
+        if delta_time >= 600000 {
+            break;
+        }
+    }
+    //trades_thread.join().unwrap();
+    wtr.flush()?;
+    Ok(())
 }
